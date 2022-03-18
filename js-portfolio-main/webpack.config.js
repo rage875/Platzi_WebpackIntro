32a481054b0fa97e4a4b461a77fb9f76c73fce41
rage875 <rage.875@gmail.com>
2022-03-18 09:25:07 -0600

    Copy of files with webpack

--- js-portfolio-main/webpack.config.js
+++ js-portfolio-main/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -25,7 +26,7 @@ module.exports = {
                 use: [
                     MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader'
                 ]
-            }
+            },
         ]
     },
     plugins: [
@@ -35,5 +36,13 @@ module.exports = {
             filename: './index.html'
         }),
         new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+              {
+                from: path.resolve(__dirname, "src", "assets/images"),
+                to: "assets/images"
+              }
+            ]
+        }),
     ]
 }
