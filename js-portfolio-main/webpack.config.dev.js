de9f29937c7793b0c2a4f68a7fdb9a68a495155c
rage875 <rage.875@gmail.com>
2022-03-21 16:07:11 -0600

    Webpack bundle analyzer

--- js-portfolio-main/webpack.config.dev.js
+++ js-portfolio-main/webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './src/index.js',
@@ -78,6 +79,7 @@ module.exports = {
             ]
         }),
         new Dotenv(),
+        new BundleAnalyzerPlugin(),
     ],
     devServer: {
         static: path.join(__dirname, 'dist'),
