0206191396c714e435dd082824a2484e2b174298
rage875 <rage.875@gmail.com>
2022-03-21 15:59:06 -0600

    Webpack dev server

--- js-portfolio-main/webpack.config.dev.js
+++ js-portfolio-main/webpack.config.dev.js
@@ -79,4 +79,10 @@ module.exports = {
         }),
         new Dotenv(),
     ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3000,
+    },
 }
